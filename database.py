1feeb7f78edde71ed945725bcf460829e0e96959
DINESH PAL <dinesh123pal@outlook.com>
2025-06-06 22:26:31 +0530

    Update database connection URL to use remote PostgreSQL instance

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE = 'postgresql://postgres:admin@localhost:5432/quiz'
+URL_DATABASE = 'postgresql://admin:rIC04df53bd5jFSHgYjMcIF2xI2ZkF2C@dpg-d11hm6vfte5s739892qg-a.oregon-postgres.render.com/et_ntiz'
 
 engine = create_engine(URL_DATABASE)
 
